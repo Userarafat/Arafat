fa4d33a34418c1223635ca6bf06b58b67986888b
Userarafat <arafat.bot.33@gmail.com>
2024-10-16 23:17:44 +0600

    Update npx2.js

--- scripts/commands/npx2.js
+++ scripts/commands/npx2.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      "https://i.imgur.com/fCQXTNF.mp4",
+      "https://i.imgur.com/9OZuVww.mp4",
       { responseType: 'stream' }
     )
   ).data;
